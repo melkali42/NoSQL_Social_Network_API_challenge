0d9d67ba46ac1bf542d925103b593df9d9f1e54b
Melissa <melissakalish2@gmail.com>
2023-09-17 14:35:42 -0500

    added body-parser connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 mongoose.connect(
     process.env.MONGOBD_URI || "mongodb://localhost/social-network",
